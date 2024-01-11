9d9642fa3c5967ee2df6a0e6949f7d08c976ee07
Mustafa Can Ayter <148382629+mustafacanayter@users.noreply.github.com>
2024-01-11 16:34:56 -0600

    Uploading api_keys.py with an updated API key.

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "2d3952120081345f0db6f706c2617220"
+weather_api_key = "beb2637993ab05662d743c52f5c57404"
 
 # Geoapify API Key
 geoapify_key = "2d3952120081345f0db6f706c2617220"
